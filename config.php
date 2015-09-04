29a6b31c72c1eb78ce24f7c44700d5a87985e225
Divakar Kalra <divakarkalra@hotmail.com>
2015-09-04 19:20:56 +0530

    first

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'os_new');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'odb_');
+
+define('REL_WEB_URL', '/bookvale/');
+
+define('WEB_PATH', 'http://localhost/bookvale/');
+
+?>
